6f82142b70b92edd672dea0c42ad1f2d909a2ae6
Aalwattar <4478356+Aalwattar@users.noreply.github.com>
2026-02-14 04:21:28 -0500

    test(win32): Add single instance tests and update plan

--- tests/test_single_instance.py
+++ tests/test_single_instance.py
@@ -1,5 +1,5 @@
-import ctypes
 from engine.platform_win.instance import SingleInstance
+
 
 def test_single_instance_detection():
     # 1. Create first instance
@@ -11,13 +11,13 @@ def test_single_instance_detection():
     # 2. Try to create second instance with same name
     instance2 = SingleInstance(mutex_name)
     assert instance2.already_running is True
-    
+
     # Clean up instance2 (instance1 still holds the mutex)
     del instance2
-    
+
     # 3. Clean up instance1 and verify a new one can be created
     del instance1
-    
+
     instance3 = SingleInstance(mutex_name)
     assert instance3.already_running is False
     del instance3
